a20c65c56eeb80f7025f27e106e9c4eca1125d05
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-08-29 13:30:23 +0700

    new commit

--- resources/views/inspeksi/p3k/create.blade.php
+++ resources/views/inspeksi/p3k/create.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard Peralatan Tanggap Darurat UP2WVI</title>
+    <title>Dashboard Peralatan Tanggap Darurat UP2WVI - P3K</title>
     <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/9/97/Logo_PLN.png" type="image/png" />
     <script src="https://cdn.tailwindcss.com"></script>
     @vite('resources/css/app.css')
