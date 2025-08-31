9e94cf82b0f0d56bd10b09d06c8da76112a80985
alfazflh <145105001+alfazflh@users.noreply.github.com>
2025-08-31 13:32:37 +0700

    new commit

--- resources/views/inspeksi/dokumen/create.blade.php
+++ resources/views/inspeksi/dokumen/create.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Dokumen IKA - PLN PUSHARLIS UP2WVI</title>
+    <title>Dashboard Dokumen IKA UP2WVI</title>
     <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/9/97/Logo_PLN.png" type="image/png" />
     @vite('resources/css/app.css')
     <style>
